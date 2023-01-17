83bc6bb5bee6023064f3888edf7af404686c514b
Bhagyaraju Pati <bhagyaraju.pati@kore.com>
2023-01-17 22:08:09 +0530

    post creating and save to DB

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 export default (posts = []/* state*/, action) => {
     switch (action.type) {
         case 'CREATE':
-            return posts;
+            return [...posts, action.payload];
 
         case 'FETCH_ALL':
             return action.payload;
